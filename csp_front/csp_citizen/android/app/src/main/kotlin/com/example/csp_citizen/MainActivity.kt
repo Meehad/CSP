4a61bd9e5ba6f4a7dc0902ed440fe9e30b76c1ed
Meehad <mohammedmihad2@gmail.com>
2024-01-29 17:00:00 +0530

    first commit

---
+++ csp_front/csp_citizen/android/app/src/main/kotlin/com/example/csp_citizen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.csp_citizen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ csp_front/csp_dept/android/app/src/main/kotlin/com/example/csp_dept/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.csp_dept
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

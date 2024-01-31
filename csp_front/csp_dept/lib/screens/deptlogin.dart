20660817ed06938bae59c7f134345efe90c182fb
Meehad <mohammedmihad2@gmail.com>
2024-01-31 13:27:08 +0530

    Fixed login page pixel issues

--- csp_front/csp_dept/lib/screens/deptlogin.dart
+++ csp_front/csp_dept/lib/screens/deptlogin.dart
@@ -1,79 +1,74 @@
 import 'package:flutter/material.dart';
 
-
-
-
 // ignore: camel_case_types
-class deptlogin  extends StatelessWidget {
+class deptlogin extends StatelessWidget {
   const deptlogin({super.key});
-  
-
+// backgroundColor: const Color.fromARGB(255, 106, 233, 110),
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 106, 233, 110),
-      body: SafeArea(
-            child: Center(
-              child: Column(
-                         children: [
-                           SizedBox(height: 10),
-
-                      Icon(Icons.login_sharp,
-                      size:75,
-                      ),
-                      SizedBox(height: 50),
-
-                      Text("WELCOME TO CSP",
-                           style: TextStyle(
-                           color: Color.fromARGB(179, 253, 251, 251),
-                           fontSize: 25
-                           ),
-                      ),
-
-                      
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                         child: Image(image: AssetImage('assets/logo.png')
-                          
-                          ),
-                        ),
-                     
-
-                    
-
-                      Padding
-                      (padding: EdgeInsets.symmetric(horizontal: 25.0),
-                      child: TextField(
-                        decoration: InputDecoration(
+    return Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              "WELCOME TO CSP",
+              style: TextStyle(
+                  color: Color.fromARGB(179, 253, 251, 251), fontSize: 25),
+            ),
+          ),
+          backgroundColor: Colors.green,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Image(
+                    image: AssetImage('assets/logo.png'),
+                    height: 100,
+                  ),
+                ),
+                const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      decoration: InputDecoration(
                           enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                          ),
-
+                              borderSide: BorderSide(color: Colors.white)),
                           focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                          ),
+                              borderSide: BorderSide(color: Colors.white)),
                           fillColor: Colors.white,
                           filled: true,
-                          hintText: "DEPARTMENT ID / ADMIN ID",
-                          hintStyle: TextStyle(color: Color.fromARGB(255, 158, 158, 158))
-                        ),
-                        obscureText: false,
-                      )
-                      ),                   
-               
-              
-                         ],
-              
-                    ),
+                          hintText: "DEPARTMENT ID",
+                          hintStyle: TextStyle(
+                              color: Color.fromARGB(255, 158, 158, 158))),
+                      obscureText: false,
+                    )),
+                const SizedBox(height: 20),
+                const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintText: "PASSWORD",
+                          hintStyle: TextStyle(
+                              color: Color.fromARGB(255, 158, 158, 158))),
+                      obscureText: false,
+                    )),
+                const SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: ElevatedButton(
+                      onPressed: () {}, child: const Text('Log in')),
+                ),
+              ],
             ),
-
-
-
-
-
-
-
-      )
-    );
+          ),
+        ));
   }
 }

53f35293a78c776e31d645ef58ecc309f65e611c
Meehad <mohammedmihad2@gmail.com>
2024-03-04 15:32:30 +0530

    fixed dept homepage oreitation

--- csp_front/csp_dept/lib/screens/depthome.dart
+++ csp_front/csp_dept/lib/screens/depthome.dart
@@ -1,3 +1,142 @@
+// import 'package:csp_dept/screens/deptdetails.dart';
+// import 'package:csp_dept/screens/deptevent.dart';
+// import 'package:csp_dept/screens/deptform.dart';
+// import 'package:csp_dept/screens/deptviewdata.dart';
+// import 'package:flutter/material.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+
+// class Depthome extends StatefulWidget {
+//   const Depthome({Key? key});
+
+//   @override
+//   State<Depthome> createState() => _DepthomeState();
+// }
+
+// class _DepthomeState extends State<Depthome> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.teal[400],
+//       appBar: AppBar(
+//         automaticallyImplyLeading: false,
+//         title: Padding(
+//           padding: const EdgeInsets.only(left: 60.0),
+//           child: Center(
+//             child: Text(
+//               'CSP',
+//               style: TextStyle(color: Colors.black, fontSize: 25),
+//             ),
+//           ),
+//         ),
+//         actions: [
+//           IconButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/dept_details');
+//             },
+//             icon: const Icon(
+//               Icons.list,
+//               color: Colors.black,
+//             ),
+//             iconSize: 45.0,
+//           )
+//         ],
+//         backgroundColor: Colors.white,
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             const SizedBox(height: 50),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.white,
+//                 onPrimary: Colors.teal[900],
+//                 elevation: 5,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//               ),
+//               onPressed: () {
+//                 Navigator.pushNamed(context, '/survey');
+//               },
+//               child: Padding(
+//                 padding: const EdgeInsets.only(
+//                     left: 90, right: 90, top: 15, bottom: 15),
+//                 child:
+//                     const Text('CREATE FORM', style: TextStyle(fontSize: 20)),
+//               ),
+//             ),
+//             const SizedBox(height: 50),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.white,
+//                 onPrimary: Colors.teal[900],
+//                 elevation: 5,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//               ),
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(builder: (context) => const deptevent()),
+//                 );
+//               },
+//               child: Padding(
+//                 padding:
+//                     EdgeInsets.only(left: 90, right: 90, top: 15, bottom: 15),
+//                 child: const Text('PUBLISH AN EVENT',
+//                     style: TextStyle(fontSize: 20)),
+//               ),
+//             ),
+//             const SizedBox(height: 50),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.white,
+//                 onPrimary: Colors.teal[900],
+//                 elevation: 5,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(20),
+//                 ),
+//               ),
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                   MaterialPageRoute(builder: (context) => const DeptViewData()),
+//                 );
+//               },
+//               child: Padding(
+//                 padding:
+//                     EdgeInsets.only(left: 90, right: 90, top: 15, bottom: 15),
+//                 child: const Text('VIEW DATA', style: TextStyle(fontSize: 20)),
+//               ),
+//             ),
+//             const SizedBox(height: 50),
+//             CarouselSlider(
+//               options: CarouselOptions(
+//                 height: 300.0,
+//                 enlargeCenterPage: true,
+//                 autoPlay: true,
+//                 aspectRatio: 16 / 9,
+//                 autoPlayCurve: Curves.fastOutSlowIn,
+//                 enableInfiniteScroll: true,
+//                 autoPlayAnimationDuration: const Duration(milliseconds: 800),
+//                 viewportFraction: 0.9,
+//               ),
+//               items: [
+//                 Image.network(
+//                   'https://images.moneycontrol.com/static-mcnews/2021/04/P-Vijayan-770x433.jpg?impolicy=website&width=770&height=431',
+//                   fit: BoxFit.cover,
+//                 ),
+//                 Image.network(
+//                   'https://1.bp.blogspot.com/-FwixDe2oaT0/YNidS5zzGEI/AAAAAAAA7_E/roj_M-86vcEG6wZUM0pSflsFkBBH7M9iwCLcBGAsYHQ/s0/govt-of-kerala-mobile-apps.webp',
+//                   fit: BoxFit.cover,
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:csp_dept/screens/deptdetails.dart';
 import 'package:csp_dept/screens/deptevent.dart';
 import 'package:csp_dept/screens/deptform.dart';
@@ -19,19 +158,21 @@ class _DepthomeState extends State<Depthome> {
       backgroundColor: Colors.teal[400],
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 60.0),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 60.0),
           child: Center(
             child: Text(
               'CSP',
-              style: TextStyle(color: Colors.black, fontSize: 25),
+              style: TextStyle(color: Colors.white, fontSize: 25),
             ),
           ),
         ),
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.pushNamed(context, '/dept_details');
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => deptdetails()),
+              );
             },
             icon: const Icon(
               Icons.list,
@@ -40,7 +181,7 @@ class _DepthomeState extends State<Depthome> {
             iconSize: 45.0,
           )
         ],
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.teal[400],
       ),
       body: Center(
         child: Column(
@@ -58,14 +199,13 @@ class _DepthomeState extends State<Depthome> {
               onPressed: () {
                 Navigator.pushNamed(context, '/survey');
               },
-              child: Padding(
-                padding: const EdgeInsets.only(
-                    left: 90, right: 90, top: 15, bottom: 15),
-                child:
-                    const Text('CREATE FORM', style: TextStyle(fontSize: 20)),
+              child: const Padding(
+                padding:
+                    EdgeInsets.only(left: 90, right: 90, top: 15, bottom: 15),
+                child: Text('CREATE FORM', style: TextStyle(fontSize: 20)),
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 30),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 primary: Colors.white,
@@ -80,14 +220,13 @@ class _DepthomeState extends State<Depthome> {
                   MaterialPageRoute(builder: (context) => const deptevent()),
                 );
               },
-              child: Padding(
+              child: const Padding(
                 padding:
-                    EdgeInsets.only(left: 90, right: 90, top: 15, bottom: 15),
-                child: const Text('PUBLISH AN EVENT',
-                    style: TextStyle(fontSize: 20)),
+                    EdgeInsets.only(left: 65, right: 65, top: 15, bottom: 15),
+                child: Text('PUBLISH AN EVENT', style: TextStyle(fontSize: 20)),
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 30),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 primary: Colors.white,
@@ -102,13 +241,13 @@ class _DepthomeState extends State<Depthome> {
                   MaterialPageRoute(builder: (context) => const DeptViewData()),
                 );
               },
-              child: Padding(
+              child: const Padding(
                 padding:
-                    EdgeInsets.only(left: 90, right: 90, top: 15, bottom: 15),
-                child: const Text('VIEW DATA', style: TextStyle(fontSize: 20)),
+                    EdgeInsets.only(left: 105, right: 105, top: 15, bottom: 15),
+                child: Text('VIEW DATA', style: TextStyle(fontSize: 20)),
               ),
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 40),
             CarouselSlider(
               options: CarouselOptions(
                 height: 300.0,

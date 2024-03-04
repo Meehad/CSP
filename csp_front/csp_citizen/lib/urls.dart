ddd3d302f2b6b96ad3e28ab8429f97e3f1801789
Meehad <mohammedmihad2@gmail.com>
2024-03-04 19:35:25 +0530

    added icons

--- csp_front/csp_citizen/lib/urls.dart
+++ csp_front/csp_citizen/lib/urls.dart
@@ -12,11 +12,5 @@ Uri postfeed = Uri.parse("http://10.0.2.2:8000/feedback/feed/");
 Uri showEvents = Uri.parse("http://10.0.2.2:8000/feedback/events_view/");
 
 Uri logouturl = Uri.parse("http://10.0.2.2:8000/csp_log/logout_user/");
-// Uri loginurl = Uri.parse("http://192.168.1.25:8000/csp_log/clogin/");
-
-// Uri profileurl =
-//     Uri.parse("http://192.168.1.25:8000/csp_log/2012022022/showprofile");
-
-// Uri logouturl = Uri.parse("http://192.168.1.25:8000/citizen/logout_user/");
 
 Uri complainturl = Uri.parse("http://10.0.2.2:8000/complaint/cpost/");

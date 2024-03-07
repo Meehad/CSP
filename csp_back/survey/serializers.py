83fb3b80ebe900b00883821d4e1523a7acf9577d
Meehad <mohammedmihad2@gmail.com>
2024-03-07 21:40:07 +0530

    api fixes

--- csp_back/survey/serializers.py
+++ csp_back/survey/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
-from .models import Survey_Q,Survey_A
+from .models import Survey_Q, Survey_A
 
 # Create your views here.
+
+
 class SurveyqSerializer(serializers.ModelSerializer):
     class Meta:
         model = Survey_Q
-        fields = '__all__'
+        fields = ['name', 'question', 'is_options', 'options', 'time']
+
 
 class SurveyaSerializer(serializers.ModelSerializer):
     class Meta:

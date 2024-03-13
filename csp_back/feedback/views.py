b4850ee94a297169bc808c27f2732d01b8766962
Meehad <mohammedmihad2@gmail.com>
2024-03-13 14:13:42 +0530

    filtering feedback at api

--- csp_back/feedback/views.py
+++ csp_back/feedback/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from csp_log.models import UserProfile
 from .models import event, feedback
 from .serializers import EventSerializer, FeedbackSerializer
 from rest_framework import status
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
 @api_view(['POST'])
 def Review_rate(request):
     print(request.data)
@@ -15,6 +19,7 @@ def Review_rate(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def Event_post(request):
@@ -38,3 +43,35 @@ def Feedbacks_view(request):
     feeds = feedback.objects.all()
     serializer = FeedbackSerializer(feeds, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def feedbacktobeanswered(request, pk):
+    try:
+        # Get the UserProfile object for the given id_number
+        user_profile = get_object_or_404(UserProfile, id_number=pk)
+
+        # Filter the feedback for the specified user
+        user_feedback = feedback.objects.filter(id_number=user_profile)
+
+        if user_feedback:
+            given_feedback_events = [a.event_name.id for a in user_feedback]
+
+            # Exclude events with feedback from all available events
+            unanswered_events = event.objects.exclude(
+                id__in=given_feedback_events)
+
+            # Serialize the unanswered events
+            serializer = EventSerializer(unanswered_events, many=True)
+
+            return Response(serializer.data)
+        else:
+            # Return all events if there is no feedback from the user
+            all_events = event.objects.all()
+            serializer = EventSerializer(all_events, many=True)
+            return Response(serializer.data)
+
+    except Http404:
+        return Response({"error": "UserProfile not found for the given id_number."}, status=404)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)

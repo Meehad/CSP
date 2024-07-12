266423779a933b456133dee435c3e341c85f1a31
Meehad <mohammedmihad2@gmail.com>
2024-07-12 16:43:48 +0530

    update

--- csp_back/csp_back/settings.py
+++ csp_back/csp_back/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-10w6a_0y!2a51gjp9&&pwbf3u37n#8nis31fl7-kktjqh%cy+i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.137.1", "192.168.0.187", "192.168.1.3", "10.0.2.2", "192.168.8.97", "192.168.88.231", "192.168.137.1",
-                 "127.0.0.1", '0.0.0.0', '192.168.1.25', '192.168.99.231', '192.168.1.4', '192.168.1.49', '192.168.1.8']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
